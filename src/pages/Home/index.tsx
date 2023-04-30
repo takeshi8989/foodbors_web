9c4cc0da479079adf2fd6e821a5925ffdc80c2b4
takeshi8989 <htakeshi0614@gmail.com>
2023-04-29 19:42:10 -0700

    created profile page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,7 +13,6 @@ const Home : () => JSX.Element  = () => {
             ))}
         </div>
     </div>
-
   )
 }
 
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,130 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 const Profile = () => {
+  const [user, setUser] = useState({
+    username: 'JohnDoe',
+    email: 'johndoe@example.com',
+    phone_number: '123-456-7890',
+    address: '123 Main St. Anytown, USA',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla ut nulla vitae ullamcorper. In ut nisi vel odio pharetra consectetur.',
+  })
+  const [formData, setFormData] = useState({
+    username: user.username,
+    email: user.email,
+    phone_number: user.phone_number,
+    address: user.address,
+    bio: user.bio,
+  })
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleTextAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = event.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  // update user info
+  const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setUser({
+      ...user,
+      ...formData,
+    })
+  }
   return (
-    <div>Profile Page</div>
+    <div className="max-w-2xl mx-auto py-12 sm:px-6 lg:px-8">
+      <div className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
+        <h2 className="text-xl font-bold mb-4">Edit Profile</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formData.username}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="phone_number">
+              Phone Number
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="phone_number"
+              type="tel"
+              name="phone_number"
+              placeholder="Phone Number"
+              value={formData.phone_number}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="address">
+              Address
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="address"
+              type="text"
+              name="address"
+              placeholder="Address"
+              value={formData.address}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="bio">
+              Bio
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="bio"
+              name="bio"
+              placeholder="Tell us about yourself..."
+              value={formData.bio}
+              onChange={handleTextAreaChange}
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-end">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Save Changes
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
